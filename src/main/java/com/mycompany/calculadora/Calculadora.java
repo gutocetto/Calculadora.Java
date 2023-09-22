1c2f9de508b1c25435ce93f3754617de183c001d
Augusto Belotto <guto.cetto@hotmail.com>
2023-09-21 21:13:58 -0300

    Criação da tela da Calculadora

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 08152
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setLocationRelativeTo(calc);
+        calc.setTitle("Calculadora Científica");
+        calc.setBackground(Color.yellow);
+        calc.setVisible(true);
+    }
+}
